dd805aebaf6bb876974a96fc7be5d701a3defa48
Sajedah.93 <your_email@whatever.comcclear>
2017-01-07 09:43:31 +0200

    Second

--- ds-stack/ds-stack.js
+++ ds-stack/ds-stack.js
@@ -1,3 +1,21 @@
 var Stack = function() {
-	//your code is here
+  this.counter = 0;
+  this.theStore = {};
 };
+
+Stack.prototype.add = function(value) {
+	this.theStore[this.counter]=value;
+	this.counter++;
+	return value;
+}
+
+Stack.prototype.remove = function() {
+	var storeageee = this.theStore[this.counter-1];
+	delete this.theStore[--this.counter]
+	return storeageee
+
+}
+Stack.prototype.storage = function() {
+return Object.keys(this.theStore).length
+};
+
--- js-array-extensions/js-array-extensions.js
+++ js-array-extensions/js-array-extensions.js
@@ -1,12 +1,12 @@
-// var Array = function(){
+ var Array = function(){
 
 
-// }
+ }
 
 Array.prototype.first = function(){
-	return Array[0]
+	return this[0]
 }
 
 Array.prototype.last = function(){
-	return Array[Array.length-1]
+	return this[this.length-1]
 }
